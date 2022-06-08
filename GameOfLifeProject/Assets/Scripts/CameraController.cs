5cc266a4a3577f0f4092940a55024fcc8471ffd7
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-08 16:02:11 +0100

    Implemented camera movement and added an empty CursorTracker class.

--- GameOfLifeProject/Assets/Scripts/CameraController.cs
+++ GameOfLifeProject/Assets/Scripts/CameraController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float xMin = 5;
+    public float xMax = 510;
+    public float yMin = 5;
+    public float yMax = 510;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x + xAxisValue, xMin, xMax),
+            Mathf.Clamp(transform.position.y + zAxisValue, yMin, yMax),
+            transform.position.z);
     }
 }
