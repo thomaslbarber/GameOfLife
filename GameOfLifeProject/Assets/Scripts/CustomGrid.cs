e666f0fe38ded509d1794b9f5fb12af19eca669c
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-09 01:16:26 +0100

    Improved code readability by reducing function sizes and adding more comments.

--- GameOfLifeProject/Assets/Scripts/CustomGrid.cs
+++ GameOfLifeProject/Assets/Scripts/CustomGrid.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// Class for managing the grid of cells.
+/// </summary>
 public class CustomGrid : MonoBehaviour
 {
     public int size_x;
     public int size_y;
 
-    // stores all the nodes in the grid
-    public Node[,] grid;
-    Node[,] gridCopy;
+    // Stores all the nodes in the grid
+    public Cell[,] grid;
+    Cell[,] gridCopy;
 
     public Tilemap tilemap;
     public Tile aliveTile;
@@ -24,6 +27,9 @@ public class CustomGrid : MonoBehaviour
     CursorTracker ct = new CursorTracker();
     Vector3Int gridPos;
 
+    /// <summary>
+    /// Start() is called before the first frame. Initialises necessary variables and sets the initial grid to be randomised.
+    /// </summary>
     private void Start()
     {
         runSimulation = false;
@@ -35,66 +41,98 @@ public class CustomGrid : MonoBehaviour
         OutputGrid();
     }
 
-
+    /// <summary>
+    /// Update is called every frame.
+    /// </summary>
     private void Update()
+    {
+        CheckForKeyboardInputs();
+
+        if (runSimulation) { return; }
+
+        CheckForMouseInput();
+    }
+
+    /// <summary>
+    /// Starts/stops the game via the space bar if it is pressed.
+    /// </summary>
+    void CheckForKeyboardInputs()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (runSimulation) { StopGame(); }
             else { StartGame(); }
         }
+    }
 
-        if (runSimulation) { return; }
-
+    /// <summary>
+    /// Checks for mouse input, adding a alive/dead cell at the mouse cursor position.
+    /// </summary>
+    void CheckForMouseInput()
+    {
+        // Place an alive cell at the cursor position.
         if (Input.GetMouseButton(0))
         {
             if (EventSystem.current.IsPointerOverGameObject()) { return; }
 
             gridPos = ConvertToGridPosition(ct.GetMousePosition());
+            grid[gridPos.y, gridPos.x].SetState(Cell.State.Alive);
 
-            grid[gridPos.y, gridPos.x].SetState(Node.State.Alive);
             OutputGrid();
         }
 
+        // Place a dead cell at the cursor position.
         if (Input.GetMouseButton(1))
         {
             if (EventSystem.current.IsPointerOverGameObject()) { return; }
 
             gridPos = ConvertToGridPosition(ct.GetMousePosition());
+            grid[gridPos.y, gridPos.x].SetState(Cell.State.Dead);
 
-            grid[gridPos.y, gridPos.x].SetState(Node.State.Dead);
             OutputGrid();
         }
     }
 
+
+    /// <summary>
+    /// Converts a given Vector3 position into a position on the grid (as a Vector3Int).
+    /// </summary>
+    /// <param name="position">The position to convert.</param>
+    /// <returns>The new Vector3Int position in the grid.</returns>
     Vector3Int ConvertToGridPosition(Vector3 position)
     {
         return gridUnity.WorldToCell(position);
     }
 
-    // generates a grid of the given size consisting of nodes
+    /// <summary>
+    /// Generates a grid of the given size consisting of cells.
+    /// </summary>
     public void CreateGrid()
     {
         runSimulation = false;
-        grid = new Node[size_x, size_y];
-        gridCopy = new Node[size_x, size_y];
+        grid = new Cell[size_x, size_y];
+        gridCopy = new Cell[size_x, size_y];
 
         for (int x = 0; x < size_x; x++)
         {
             for (int y = 0; y < size_y; y++)
             {
-                grid[x, y] = new Node(RandomiseNodeState(), x, y);
-                gridCopy[x, y] = new Node(grid[x, y].GetState(), x, y);
+                grid[x, y] = new Cell(RandomiseCellState(), x, y);
+                gridCopy[x, y] = new Cell(grid[x, y].GetState(), x, y);
             }
         }
 
         OutputGrid();
     }
 
+    /// <summary>
+    /// Sets every cell in the grid to be of the same state (alive/dead).
+    /// </summary>
+    /// <param name="isAlive">Whether to generate the grid as alive cells or dead cells.</param>
     public void CreateIdenticalGrid(bool isAlive)
     {
         runSimulation = false;
-        Node.State state = isAlive ? Node.State.Alive : Node.State.Dead;
+        Cell.State state = isAlive ? Cell.State.Alive : Cell.State.Dead;
 
         for (int x = 0; x < size_x; x++)
         {
@@ -107,21 +145,32 @@ public class CustomGrid : MonoBehaviour
         OutputGrid();
     }
 
-    Node.State RandomiseNodeState()
+    /// <summary>
+    /// Generates a random state for a cell.
+    /// </summary>
+    /// <returns>Returns the random state.</returns>
+    Cell.State RandomiseCellState()
     {
-        int i = Random.Range(0, 16);
-        if (i == 0) { return Node.State.Alive; }
-        return Node.State.Dead;
+        int n = Random.Range(0, 16);
+        if (n == 0) { return Cell.State.Alive; }
+        return Cell.State.Dead;
     }
 
+    /// <summary>
+    /// Enum for whether the simulation is currently active or inactive.
+    /// </summary>
     enum SimulationStatus
     {
         Active,
-        NotActive,
+        Inactive,
     }
 
-    SimulationStatus sStatus = SimulationStatus.NotActive;
+    SimulationStatus sStatus = SimulationStatus.Inactive;
     bool runSimulation = true;
+
+    /// <summary>
+    /// Starts the simulation.
+    /// </summary>
     public void StartGame()
     {
         if (sStatus == SimulationStatus.Active) { return; }
@@ -131,15 +180,22 @@ public class CustomGrid : MonoBehaviour
         StartCoroutine(GenerateGridLoop());
     }
 
+    /// <summary>
+    /// Stops the simulation.
+    /// </summary>
     public void StopGame()
     {
         runSimulation = false;
     }
 
-    // returns a list of all neighbours of a node
-    public List<Node> Neighbours(Node node)
+    /// <summary>
+    /// Returns a list of all neighbours of a given cell.
+    /// </summary>
+    /// <param name="cell">The cell to get the neighbours of.</param>
+    /// <returns>Returns a list of all neighbours of a cell.</returns>
+    public List<Cell> Neighbours(Cell cell)
     {
-        List<Node> neighbours = new List<Node>();
+        List<Cell> neighbours = new List<Cell>();
 
         for (int x = -1; x <= 1; x++)
         {
@@ -147,8 +203,8 @@ public class CustomGrid : MonoBehaviour
             {
                 if (x == 0 && y == 0) { continue; }
 
-                int checkX = node.XPos + x;
-                int checkY = node.YPos + y;
+                int checkX = cell.XPos + x;
+                int checkY = cell.YPos + y;
 
                 if (checkX >= 0 && checkX < size_x && checkY >= 0 && checkY < size_y)
                 {
@@ -160,7 +216,9 @@ public class CustomGrid : MonoBehaviour
         return neighbours;
     }
 
-    // outputs the current grid to the console
+    /// <summary>
+    /// Outputs the current grid to the console.
+    /// </summary>
     public void OutputGrid()
     {
         for (int i = 0; i < size_y; i++)
@@ -168,7 +226,7 @@ public class CustomGrid : MonoBehaviour
             for (int j = 0; j < size_x; j++)
             {
                 Vector3Int p = new Vector3Int(i, j, 0);
-                if (grid[j, i].GetState() == Node.State.Alive)
+                if (grid[j, i].GetState() == Cell.State.Alive)
                 {
                     tilemap.SetTile(p, aliveTile);
                 }
@@ -182,22 +240,24 @@ public class CustomGrid : MonoBehaviour
         }
     }
 
-    // updates the grid according to the rules of the game of life
+    /// <summary>
+    /// Updates the grid according to the rules of the game of life.
+    /// </summary>
     public void UpdateGrid()
     {
-        Node[,] newGrid = new Node[size_x, size_y];
+        Cell[,] newGrid = new Cell[size_x, size_y];
 
         for (int x = 0; x < size_x; x++)
         {
             for (int y = 0; y < size_y; y++)
             {
-                List<Node> neighbours = Neighbours(grid[x, y]);
+                List<Cell> neighbours = Neighbours(grid[x, y]);
 
                 int neighboursAlive = 0;
                 int neighboursDead = 0;
-                foreach (Node neighbour in neighbours)
+                foreach (Cell neighbour in neighbours)
                 {
-                    if (neighbour.GetState() == Node.State.Alive)
+                    if (neighbour.GetState() == Cell.State.Alive)
                     {
                         neighboursAlive++;
                     }
@@ -213,31 +273,30 @@ public class CustomGrid : MonoBehaviour
                  * 3. Else live cell dies
                  */
 
-                if (grid[x, y].GetState() == Node.State.Alive)
+                if (grid[x, y].GetState() == Cell.State.Alive)
                 {
                     if (neighboursAlive == 2 || neighboursAlive == 3)
                     {
                         // survives
-                        newGrid[x, y] = new Node(Node.State.Alive, x, y);
+                        newGrid[x, y] = new Cell(Cell.State.Alive, x, y);
                     }
                     else
                     {
                         // dies
-                        newGrid[x, y] = new Node(Node.State.Dead, x, y);
+                        newGrid[x, y] = new Cell(Cell.State.Dead, x, y);
                     }
                 }
                 else
                 {
                     if (neighboursAlive == 3)
                     {
-                        newGrid[x, y] = new Node(Node.State.Alive, x, y);
+                        newGrid[x, y] = new Cell(Cell.State.Alive, x, y);
                     }
                     else
                     {
-                        newGrid[x, y] = new Node(Node.State.Dead, x, y);
+                        newGrid[x, y] = new Cell(Cell.State.Dead, x, y);
                     }
                 }
-
             }
         }
 
@@ -255,7 +314,7 @@ public class CustomGrid : MonoBehaviour
             yield return new WaitForSeconds(DELAY);
         }
 
-        sStatus = SimulationStatus.NotActive;
+        sStatus = SimulationStatus.Inactive;
         yield break;
     }
 }
