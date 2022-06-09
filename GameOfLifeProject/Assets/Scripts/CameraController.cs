67dbf583c54bff5ec0f5dfe7ab76f28c5d067fc6
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-09 01:35:08 +0100

    Adds comments to CameraController.cs and other readability improvements.

--- GameOfLifeProject/Assets/Scripts/CameraController.cs
+++ GameOfLifeProject/Assets/Scripts/CameraController.cs
@@ -3,34 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
+/// <summary>
+/// Class for handling the camera behaviour. Allows for moving (WASD) and zooming (mouse wheel).
+/// </summary>
 public class CameraController : MonoBehaviour
 {
-    // Boundaries
+    // Movement variables.
     public float xMin = 5;
     public float xMax = 510;
     public float yMin = 5;
     public float yMax = 510;
+    float moveSpeed = 2f;
 
-    [SerializeField] private CinemachineVirtualCamera cam;
-    CinemachineComponentBase camBase;
-    float camDist;
-    [SerializeField] float sens = 10f;
+    // Zoom variables.
+    [SerializeField] private CinemachineVirtualCamera cinemachineCamera;
+    private float camDist;
+    private readonly float sens = 50f;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    private void Update()
+    {
+        Zoom();
+    }
+
+    /// <summary>
+    /// FixedUpdate is called at a rate of the physics timestep.
+    /// </summary>
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Zooms the camera using the mouse scrollwheel.
+    /// </summary>
+    private void Zoom()
     {
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
             camDist = Input.GetAxis("Mouse ScrollWheel") * sens;
-            cam.m_Lens.OrthographicSize -= camDist;
-            if (cam.m_Lens.OrthographicSize < 0) { cam.m_Lens.OrthographicSize = 0; }
-            else if (cam.m_Lens.OrthographicSize > 135) { cam.m_Lens.OrthographicSize = 135; }
+            cinemachineCamera.m_Lens.OrthographicSize -= camDist;
+            if (cinemachineCamera.m_Lens.OrthographicSize < 0) { cinemachineCamera.m_Lens.OrthographicSize = 0; }
+            else if (cinemachineCamera.m_Lens.OrthographicSize > 135) { cinemachineCamera.m_Lens.OrthographicSize = 135; }
         }
+    }
 
-
-        float xAxisValue = Input.GetAxis("Horizontal");
-        float zAxisValue = Input.GetAxis("Vertical");
-
+    /// <summary>
+    /// Moves the camera using WASD.
+    /// </summary>
+    private void Move()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * moveSpeed;
+        float zAxisValue = Input.GetAxis("Vertical") * moveSpeed;
 
         transform.position = new Vector3(
             Mathf.Clamp(transform.position.x + xAxisValue, xMin, xMax),
